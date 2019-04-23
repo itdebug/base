bdfbd057c8708a23188543df52289a03f28dcc01
陆荣康 <lurongkang@21tb.com>
2019-04-23 18:25:01 +0800

    first commit

---
+++ base-web/src/main/java/com/example/base/BaseWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseWebApplication.class, args);
+    }
+
+}
